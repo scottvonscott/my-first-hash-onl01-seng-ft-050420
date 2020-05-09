7118efe95261e17cdb4f52239a94f679980ef607
Scott  Keplinger <skeplin17@gmail.com>
2020-05-09 20:44:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,6 +24,7 @@ def adding
                   "oil paintings" => 3
                  }
 shipping_manifest["muskets"] = "2"
+shipping_manifest["gun powder"] = "4"
 
 
   # add 4 gun powder to the shipping_manifest hash below
